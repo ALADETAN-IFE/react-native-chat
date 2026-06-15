5463dcc6b068f63394e94736a53177ed492bb909
ALADETAN-IFE <163364168+ALADETAN-IFE@users.noreply.github.com>
2026-06-15 22:18:20 +0100

    fix(lint): correct lint issues

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,8 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo", { jsxImportSource: "react" }]],
-    plugins: ["react-native-worklets/plugin"],
+    presets: [['babel-preset-expo', { jsxImportSource: 'react' }]],
+    plugins: ['react-native-worklets/plugin'],
   };
 };
 // REMOVED BY REPOGUARD: obfuscated malware payload
